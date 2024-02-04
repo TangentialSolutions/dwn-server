745ced1c941f2672d6cdadd4a186d649d6c966be
Trevor Broaddus <trevor.broaddus@hey.com>
2024-02-03 20:38:00 -0600

    Add run script for my local development

---
+++ tb-run.sh
@@ -0,0 +1,1 @@
+PGUSER=postgres PGPASSWORD=postgres DS_PORT=3001 DWN_STORAGE_EVENTS=postgres://0.0.0.0:5432/dwn DWN_STORAGE_MESSAGES=postgres://0.0.0.0:5432/dwn DWN_STORAGE_DATA=postgres://0.0.0.0:5432/dwn npm run server
